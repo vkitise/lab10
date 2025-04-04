48bad24626837e3c8e9b1d04dd543231e29aad50
vkitise <151126627+vkitise@users.noreply.github.com>
2025-04-04 12:23:37 +0530

    Update EmbeddedJettyServer.java

--- cookie/src/main/java/EmbeddedJettyServer.java
+++ cookie/src/main/java/EmbeddedJettyServer.java
@@ -1,18 +1,13 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-
 public class EmbeddedJettyServer {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
-
-        // Register the CookieExample servlet
         context.addServlet(new ServletHolder(new CookieExample()), "/cookie");
-
         server.setHandler(context);
-
         System.out.println("Server starting on http://localhost:8080/cookie");
         server.start();
         server.join();
