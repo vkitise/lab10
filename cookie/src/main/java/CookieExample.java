2d609ce163fa0dd005cc3d871d8f5bd8ce77ad62
vkitise <151126627+vkitise@users.noreply.github.com>
2025-04-04 12:23:20 +0530

    Update CookieExample.java

--- cookie/src/main/java/CookieExample.java
+++ cookie/src/main/java/CookieExample.java
@@ -4,18 +4,12 @@ import java.net.URL;
 import java.net.URLEncoder;
 import javax.servlet.*;
 import javax.servlet.http.*;
-
 public class CookieExample extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        // Create a new cookie with the name "EMPID" and the value "AN2356"
         Cookie cookie = new Cookie("EMPID", "AN2356");
-        // Set the maximum age of the cookie to 60 seconds
         cookie.setMaxAge(60);
-        // Add the cookie to the response
         response.addCookie(cookie);
-        
-        // Read the cookie from the response
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie c : cookies) {
